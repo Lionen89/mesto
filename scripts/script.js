eedd409f168cdcd8d5cd17691b9e88b4729ccca3
Ilya Trubitcin <lionen89@yandex.ru>
2021-11-16 09:38:05 +0300

    new card realized

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@ let oldJob = document.querySelector('.profile__description');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__text[name="name"]');
 let jobInput = document.querySelector('.popup__text[name="description"]');
+const elementTemplate = document.querySelector('.element-template');
+const elementsPlace= document.querySelector('.elements');
 
 function openPopup() {
     nameInput.value = oldName.textContent;
@@ -32,3 +34,44 @@ function formSubmitHandler(evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+// задаем 6 карточек
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+// вставляем на траницу 6 карточек
+
+initialCards.forEach((item) => {
+    createCards(item.name, item.link)
+
+});
+
+function createCards(name, link) {
+    const userElement = elementTemplate.content.querySelector('.element').cloneNode(true);
+    userElement.querySelector('.element__image').src = link;
+    userElement.querySelector('.element__image').alt = name;
+    userElement.querySelector('.element__description > .element__title').textContent = name;
+    elementsPlace.append(userElement);
+}
